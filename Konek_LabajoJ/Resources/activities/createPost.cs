4eb4eb02a6e0bcb8c54a3612f30e4fe08233b972
JericoL <jericolabajo11@gmail.com>
2024-04-01 10:56:38 +0800

    Create post camera edit 1.1

--- Konek_LabajoJ/Resources/activities/createPost.cs
+++ Konek_LabajoJ/Resources/activities/createPost.cs
@@ -1,36 +1,54 @@
-﻿using Android.App;
+﻿using Android;
+using Android.App;
 using Android.Content;
 using Android.Content.PM;
+using Android.Graphics;
 using Android.Hardware.Camera2.Params;
 using Android.OS;
 using Android.Renderscripts;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using AndroidX.AppCompat.App;
 using Plugin.Media;
 using Plugin.Media.Abstractions;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.IO;
 using System.Linq;
 using System.Text;
+using AlertDialog = AndroidX.AppCompat.App.AlertDialog;
+using Bitmap = Android.Graphics.Bitmap;
 
 namespace Konek_LabajoJ.Resources.activities
 {
 
     [Activity(Label = "createPost", Theme = "@style/AppTheme", MainLauncher = false)]
-  
 
-    public class createPost : Activity
+
+    public class createPost : AppCompatActivity
     {
 
         ImageView createPost_Back, createPostimage;
-        Button  createpostSubmit;
+        Button createpostSubmit;
         EditText createTextpost;
         TextView createpostCancel;
+
+
+        readonly string[] permissionGroup =
+        {
+               Manifest.Permission.ReadExternalStorage,
+               Manifest.Permission.WriteExternalStorage,
+               Manifest.Permission.Camera
+    };
+
+
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
+
+            //referencing
             SetContentView(Resource.Layout._createpost);
 
             
@@ -41,6 +59,10 @@ namespace Konek_LabajoJ.Resources.activities
 
             createpostCancel.Click += CreatepostCancel_Click1;
             createPostimage.Click += CreatePostimage_Click ;
+
+
+            //calling the permission method
+            RequestPermissions(permissionGroup, 0);
         }
 
         private void CreatePostimage_Click(object sender, EventArgs e)
@@ -53,31 +75,56 @@ namespace Konek_LabajoJ.Resources.activities
             });
 
             builder.SetNegativeButton("Upload Photo", (sender, e) => {
-                //     uploadPhoto();
+                //     uploadPhoto();  
             });
+
+            Dialog dialog = builder.Create();
+            dialog.Show();  
         }
 
 
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
             base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
         }
 
-        //DARIA NAKA 10 MINS
-    /*    async void takePhoto()
+        async void takePhoto()
         {
-            await CrossMedia.Current.Initialize();
-            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions){
-
+           await CrossMedia.Current.Initialize();
+            var file = await CrossMedia.Current.TakePhotoAsync(new Plugin.Media.Abstractions.StoreCameraMediaOptions
+            {
                 PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
                 CompressionQuality = 20,
                 Directory = "Sample",
-                Name = Generate
+                Name = $"{generateRandomString(5)}{DateTime.Now.ToShortDateString()}_konek.jpg"
+            }); 
+          
+            if(file == null)
+            {
+                return;
+            }
+
+            byte[] imageArray = System.IO.File.ReadAllBytes(file.Path);
+            Bitmap bitmap = BitmapFactory.DecodeByteArray(imageArray, 0, imageArray.Length);
+            createPostimage.SetImageBitmap(bitmap);
+        }
+         //18 mins na DIRI NAKA
+
+    /*    async void selectPhoto()
+      {
+            await CrossMedia.Current.Initialize();
+            var file = await CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.
+            {
+                PhotoSize = Plugin.Media.Abstractions.PhotoSize.Medium,
+                CompressionQuality = 20,
+
             });
-         }*/
 
 
-        private void CreatepostCancel_Click1(object sender, EventArgs e)
+        }*/
+
+            private void CreatepostCancel_Click1(object sender, EventArgs e)
         {
             var intent = new Intent(this, typeof(MainActivity));
 
@@ -86,6 +133,19 @@ namespace Konek_LabajoJ.Resources.activities
             StartActivity(intent);
         }
 
+        string generateRandomString(int length)
+        {
+            Random random = new Random();
+            char[] allowChars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890".ToCharArray();
+
+            string eResult = "";
+
+            for(int i =0; i < length; i++)
+            {
+                eResult += allowChars[random.Next(0, allowChars.Length)];
+            }
+            return eResult;
+        }
 
 
     }
